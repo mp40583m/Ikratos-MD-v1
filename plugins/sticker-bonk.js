fba8bad54acedb93b1aaf6daa51fa2f13a7c30a4
mp40583m <130332913+mp40583m@users.noreply.github.com>
2023-04-12 01:13:39 +0200

    Update sticker-bonk.js

--- plugins/sticker-bonk.js
+++ plugins/sticker-bonk.js
@@ -11,6 +11,6 @@ let handler = async (m, { conn, text }) => {
 		}).getBufferAsync('image/png')
 	conn.sendMessage(m.chat, { image: bonk }, { quoted: m })
 }
-handler.command = /^(bonk)$/i
+handler.command = /^(ضربة)$/i
 
 export default handler
