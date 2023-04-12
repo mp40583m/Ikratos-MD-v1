730bf94380b1a530e2181684f8c03534c10f546a
mp40583m <130332913+mp40583m@users.noreply.github.com>
2023-04-12 03:27:04 +0200

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`${text ? `${text}\n` : ''}┌─「 Tag All 」\n` + users.map(v => '│🇵🇰 @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, {
+    m.reply(`${text ? `${text}\n` : ''}┌─「 منشن جماعي 」\n` + users.map(v => '│🇪🇬 @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, {
         mentions: users
     })
 }
 
 handler.help = ['tagall']
 handler.tags = ['group']
-handler.command = ['tagall']
+handler.command = ['منشن']
 handler.admin = true
 handler.group = true
 
